f5d70690599771f23550dac6f0b5b9f4acd5dbc6
Angel Sierra <asierra.me@gmail.com>
2022-06-07 20:33:51 -0400

    Update video-streaming microservice

--- video-streaming/src/index.js
+++ video-streaming/src/index.js
@@ -1,40 +1,51 @@
 const express = require("express");
 const fs = require("fs");
-const path = require("path");
 
 const app = express();
 
 //
-// Throws an error if the PORT environment variable is missing.
+// Throws an error if the any required environment variables are missing.
 //
-if (!process.env.PORT){
-    throw new Error("Please specify the port number for the HTTP server with the enviornemt variable PORT.")
+
+if (!process.env.PORT) {
+    throw new Error("Please specify the port number for the HTTP server with the environment variable PORT.");
+}
+
+if (!process.env.VIDEO_STORAGE_HOST) {
+    throw new Error("Please specify the host name for the video storage microservice in variable VIDEO_STORAGE_HOST.");
+}
+
+if (!process.env.VIDEO_STORAGE_PORT) {
+    throw new Error("Please specify the port number for the video storage microservice in variable VIDEO_STORAGE_PORT.");
 }
 
 //
-// Extracts the PORT environment variable.
+// Extracts environment variables to globals for convenience.
 //
 const PORT = process.env.PORT;
+const VIDEO_STORAGE_HOST = process.env.VIDEO_STORAGE_HOST;
+const VIDEO_STORAGE_PORT = parseInt(process.env.VIDEO_STORAGE_PORT);
+console.log(`Forwarding video requests to ${VIDEO_STORAGE_HOST}:${VIDEO_STORAGE_PORT}.`);
 
 //
 // Registers a HTTP GET route for video streaming.
 //
 app.get("/video", (req, res) => {
-
-    const videoPath = path.join("./videos", "SampleVideo_1280x720_1mb.mp4");
-    fs.stat(videoPath, (err, stats) => {
-        if (err) {
-            console.error("An error occurred ");
-            res.sendStatus(500);
-            return;
+    const forwardRequest = http.request( // Forward the request to the video storage microservice.
+        {
+            host: VIDEO_STORAGE_HOST,
+            port: VIDEO_STORAGE_PORT,
+            path: '/video?path=SampleVideo_1280x720_1mb.mp4', // Video path is hard-coded for the moment.
+            method: 'GET',
+            headers: req.headers
+        }, 
+        forwardResponse => {
+            res.writeHeader(forwardResponse.statusCode, forwardResponse.headers);
+            forwardResponse.pipe(res);
         }
-
-        res.writeHead(200, {
-            "Content-Length": stats.size,
-            "Content-Type": "video/mp4",
-        });
-        fs.createReadStream(videoPath).pipe(res);
-    });
+    );
+    
+    req.pipe(forwardRequest);
 });
 
 //
